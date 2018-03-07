25ebd89e5068558208ca40605235fc8a171b081c
Joe Lipper <lipperjj@gmail.com>
2018-03-07 11:01:23 -0800

    Add capability to build sass separately from JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,63 @@
 const { join, resolve } = require('path');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const { getIfUtils, removeEmpty } = require('webpack-config-utils');
 
 module.exports = env => {
-  const { ifProd } = getIfUtils(env);
+  const { ifProd, ifNotProd } = getIfUtils(env);
+
+  const extractSass = new ExtractTextPlugin({
+    filename: ifProd('[name].[chunkhash].css', '[name].css')
+  });
 
   const config = {
-    context: resolve('source/javascripts'),
-    entry: './site.js', // Path is relative to the "context" path
+    context: resolve('source'),
+    entry: {
+      site: './javascripts/site.js',
+      styles: ['./stylesheets/landing.css.sass']
+    },
     output: {
-      filename: 'bundle.js',
-      path: resolve('.tmp/dist/javascripts'),
-      publicPath: '/dist/javascripts/',
+      filename: ifProd('bundle.[name].[chunkhash].js', 'bundle.[name].js'),
+      path: resolve('.tmp/dist/'),
+      pathinfo: ifNotProd(),
     },
     devtool: ifProd('source-map', 'eval'),
     module: {
-      rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                'babel-preset-env',
-                {
-                  'targets': {
-                    'browsers': ['last 2 versions', 'safari >= 7']
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  'babel-preset-env',
+                  {
+                    'targets': {
+                      'browsers': ['last 2 versions', 'safari >= 7']
+                    }
                   }
-                }
+                ]
               ]
-            ]
+            }
           }
-        }
-      }]
+        },
+        {
+          test: /\.sass$/,
+          use: extractSass.extract({
+            use: [
+              { loader: 'css-loader' },
+              { loader: 'sass-loader' }
+            ],
+            fallback: 'style-loader'
+          })
+        },
+      ]
     },
     plugins: removeEmpty([
-      new ProgressBarPlugin()
+      new ProgressBarPlugin(),
+      extractSass
     ])
   };
 
