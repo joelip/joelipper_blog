5e8043736e7881652337b16056b9096bd111f97c
Joe Lipper <lipperjj@gmail.com>
2018-03-06 17:07:02 -0800

    Add babel plugins

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,27 @@ module.exports = env => {
       publicPath: '/dist/javascripts/',
     },
     devtool: ifProd('source-map', 'eval'),
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                'babel-preset-env',
+                {
+                  'targets': {
+                    'browsers': ['last 2 versions', 'safari >= 7']
+                  }
+                }
+              ]
+            ]
+          }
+        }
+      }]
+    },
   };
 
   return config;
