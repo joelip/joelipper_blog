39dd179fa8288ca55790a3aceccfa16ac8572818
Joe Lipper <lipperjj@gmail.com>
2018-05-15 22:16:42 -0700

    Minify and gzip assets

--- config.rb
+++ config.rb
@@ -46,7 +46,8 @@ activate :syntax, inline_theme: 'github'
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :gzip
+end
